38edad3fa40cb52176d07c4248f121c507a580fb
ulif <uli@gnufix.de>
2015-05-11 17:55:37 +0200

    Make test setup work with zope testrunner.
    Looks like test setup for Zope tests is quite difficult, even if only
    very plain tests are performed.

--- src/megrok/paypal/tests/test_megrok_paypal.py
+++ src/megrok/paypal/tests/test_megrok_paypal.py
@@ -4,4 +4,10 @@ import unittest
 class TestPackage(unittest.TestCase):
 
     def test_foo(self):
-        assert 1 == 1
+        assert 1 == 0
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests(unittest.makeSuite(TestPackage))
+    return suite
