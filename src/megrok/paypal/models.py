e2a14a5659303ea993ffe67e406475b9cf07f5ff
ulif <uli@gnufix.de>
2015-06-02 13:47:26 +0200

    Let PayPalForm implement IPayPalPaymentForm.

--- src/megrok/paypal/models.py
+++ src/megrok/paypal/models.py
@@ -1,7 +1,27 @@
+import decimal
 import grok
 from megrok.paypal.interfaces import IPayPalPaymentForm
 
 
-class PayPalForm(grok.Model):
+class PayPalForm(object):
 
     grok.implements(IPayPalPaymentForm)
+
+    @property
+    def notify_url(self):
+        return None
+
+    @property
+    def return_url(self):
+        return None
+
+    @property
+    def cancel_return(self):
+        return None
+
+    def __init__(self, business=None, item_name=None,
+                  amount=decimal.Decimal("0.00"), invoice=None):
+        self.invoice = invoice
+        self.business = business
+        self.item_name = item_name
+        self.amount = amount
