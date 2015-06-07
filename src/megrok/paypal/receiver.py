67c0fa8c7c1ab9b085ff23fc30b1bfbf9f9c8dde
ulif <uli@gnufix.de>
2015-06-07 14:13:17 +0200

    Add an IPN receiver as app.

---
+++ src/megrok/paypal/receiver.py
@@ -0,0 +1,19 @@
+"""Receiver for PayPal messages like IPN.
+
+IPN infos:
+
+  https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/
+
+IPN simulator:
+
+  https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNSimulator/
+
+"""
+import grok
+from megrok.paypal.interfaces import IPayPalIPNReceiver
+
+
+class PayPalIPNReceiver(grok.Application, grok.Container):
+    """A receiver for IPN messages sent from paypal.
+    """
+    grok.implements(IPayPalIPNReceiver)
