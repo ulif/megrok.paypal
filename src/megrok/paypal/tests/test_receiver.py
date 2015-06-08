5e4aeb890f50148dbd6c745ee4236e47c54d3e91
ulif <uli@gnufix.de>
2015-06-08 21:04:41 +0200

    Add functional test layer.
    and a test to test it.

--- src/megrok/paypal/tests/test_receiver.py
+++ src/megrok/paypal/tests/test_receiver.py
@@ -1,8 +1,13 @@
 # Tests for IPN- and other receivers
 import unittest
+import megrok.paypal.tests
+from zope.app.wsgi.testlayer import BrowserLayer
 from zope.interface.verify import verifyClass, verifyObject
 from megrok.paypal.interfaces import IPayPalIPNReceiver
 from megrok.paypal.receiver import PayPalIPNReceiver
+
+
+FunctionalLayer = BrowserLayer(megrok.paypal.tests, 'ftesting.zcml')
 
 
 class TestPayPalIPNReceiver(unittest.TestCase):
@@ -12,3 +17,12 @@ class TestPayPalIPNReceiver(unittest.TestCase):
         receiver = PayPalIPNReceiver()
         verifyClass(IPayPalIPNReceiver, PayPalIPNReceiver)
         verifyObject(IPayPalIPNReceiver, receiver)
+
+
+class TestPayPalIPNReceiverFunctional(unittest.TestCase):
+
+    layer = FunctionalLayer
+
+    def test_get_rootfolder(self):
+        # TEMPORARY test. Make sure we can get a root folder.
+        assert self.layer.getRootFolder() is not None
